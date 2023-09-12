5aa2492c3b0118b8188d06438925807a961f5426
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-12 12:05:59 +0100

    removed an productlist import from app.js

--- front-end-client/src/App.js
+++ front-end-client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import ShopContainer from "./containers/ShopContainer";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import ProductList from "./components/landingComponents/ProductList";
 import Checkout from "./components/checkoutComponents/Checkout";
 import ShopHeader from "./components/ShopHeader";
 
