14e9752e14775e5c1bdc6f36b3ee6b25072ed115
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-12 16:24:24 +0100

    added other logos

--- front-end-client/src/components/ShopFooter.js
+++ front-end-client/src/components/ShopFooter.js
@@ -9,6 +9,21 @@ const ShopFooter = () => {
             className="facebook-logo logo"
           />
         </li>
+        <li>
+          <img
+            src="/images/icon-instagram.svg"
+            alt="Instagram Logo"
+            className="instagram-logo logo"
+          />
+        </li>
+        <li>
+          <img
+            src="/images/icon-linkedin.svg"
+            alt="LinkedIn Logo"
+            className="linkedin-logo logo"
+          />
+        </li>
+
       </ul>
     </footer>
   );
