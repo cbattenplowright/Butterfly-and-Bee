d790d57aac1e3bac1583e28d5c5f32e9df7f042d
rohaib1 <rohaib192@gmail.com>
2023-09-12 15:12:44 +0100

    Working search bar but needs tweaking

--- front-end-client/src/containers/ShopContainer.js
+++ front-end-client/src/containers/ShopContainer.js
@@ -1,70 +1,81 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ProductList from "../components/landingComponents/ProductList";
 import Filters from "../components/landingComponents/Filters";
+import ProductItem from "../components/landingComponents/ProductItem";
 
 const ShopContainer = () => {
-    const [productItems, setProductItems] = useState([]);
+  const [productItems, setProductItems] = useState([]);
 
-    const fetchProductItems = async () => {
-        const response = await fetch("http://localhost:8080/products");
-        const data = await response.json();
-        setProductItems(data);
-        console.log(data);
-    };
+  const fetchProductItems = async () => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    setProductItems(data);
+    console.log(data);
+  };
 
-    const fetchMovies = async () => {
-        const response = await fetch("http://localhost:8080/products/movies");
-        const data = await response.json();
-        setProductItems(data);
-        console.log(data);
-    };
+  const fetchMovies = async () => {
+    const response = await fetch("http://localhost:8080/products/movies");
+    const data = await response.json();
+    setProductItems(data);
+    console.log(data);
+  };
 
-    const fetchBooks = async () => {
-        const response = await fetch("http://localhost:8080/products/books");
-        const data = await response.json();
-        setProductItems(data);
-        console.log(data);
-    };
+  const fetchBooks = async () => {
+    const response = await fetch("http://localhost:8080/products/books");
+    const data = await response.json();
+    setProductItems(data);
+    console.log(data);
+  };
 
-    const fetchSongs = async () => {
-        const response = await fetch("http://localhost:8080/products/songs");
-        const data = await response.json();
-        setProductItems(data);
-        console.log(data);
-    };
+  const fetchSongs = async () => {
+    const response = await fetch("http://localhost:8080/products/songs");
+    const data = await response.json();
+    setProductItems(data);
+    console.log(data);
+  };
 
-    const fetchProductItemsBySearchQuery = async (searchQuery) => {
-        console.log(searchQuery);
-        const movieResponse = await fetch(`http://localhost:8080/products/movie/${searchQuery}`)
-        // const bookResponse = await fetch("http://localhost:8080/products/books/${searchQuery}")
-        // const musicResponse = await fetch("http://localhost:8080/products/songs/${searchQuery}")
-        const data = await movieResponse.json();
-        // data.push(await bookResponse.json());
-        // data.push(await musicResponse.json());
-        setProductItems(data);
-        console.log(data);
-        console.log(movieResponse);
-        
+  const fetchProductItemsBySearchQuery = async (searchQuery) => {
+    try {
+      const [movieResponse, bookResponse, musicResponse] = await Promise.all([
+        fetch(`http://localhost:8080/products/movie/${searchQuery}`),
+        fetch(`http://localhost:8080/products/books/${searchQuery}`),
+        fetch(`http://localhost:8080/products/songs/${searchQuery}`),
+      ]);
+
+  
+      const movieData = await movieResponse.json();
+      const bookData = await bookResponse.json();
+      const musicData = await musicResponse.json();
+
+
+      const combinedData = [...movieData, ...bookData, ...musicData];
+
+      setProductItems(combinedData);
+    } catch (error) {
+      console.error('No Product found', error);
     }
+  };
 
-    useEffect(() => {
-        fetchProductItems();
-    }, []);
+  useEffect(() => {
+    fetchProductItems();
+  }, []);
 
-
-    return (
-        <>
-            <h1>Hello World!</h1>
-            <Filters
-                fetchMovies={fetchMovies}
-                fetchBooks={fetchBooks}
-                fetchSongs={fetchSongs}
-                fetchProductItems={fetchProductItems}
-                fetchProductItemsBySearchQuery={fetchProductItemsBySearchQuery}
-            />
-            <ProductList productItems={productItems} />
-        </>
-    );
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Filters
+        fetchMovies={fetchMovies}
+        fetchBooks={fetchBooks}
+        fetchSongs={fetchSongs}
+        fetchProductItems={fetchProductItems}
+        fetchProductItemsBySearchQuery={fetchProductItemsBySearchQuery}
+      />
+      {/* */}
+      {productItems.map((productItem) => (
+        <ProductItem key={productItem.id} productItem={productItem} />
+      ))}
+    </>
+  );
 };
 
 export default ShopContainer;
