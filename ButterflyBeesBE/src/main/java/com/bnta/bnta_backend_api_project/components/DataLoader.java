5970d6be0f264fb917d4c6804d2262d94bab9989
rohaib1 <rohaib192@gmail.com>
2023-09-13 10:12:04 +0100

    Amended backend to add images

--- ButterflyBeesBE/src/main/java/com/bnta/bnta_backend_api_project/components/DataLoader.java
+++ ButterflyBeesBE/src/main/java/com/bnta/bnta_backend_api_project/components/DataLoader.java
@@ -44,41 +44,41 @@ public class DataLoader implements ApplicationRunner {
         // PRODUCTS DATA LOADER
 
         List<Book> books = new ArrayList<>(Arrays.asList(
-                new Book("JK Rowling", "Harry Potter and the Philosopher's Stone", 13, 11, 40, 2001, 500),
-                new Book("George Orwell", "1984", 43, 11, 30, 1949, 350),
-                new Book("J.R.R. Tolkien", "The Lord of the Rings", 12, 11, 20, 1954, 1170),
-                new Book("Agatha Christie", "Murder on the Orient Express", 533, 11, 10, 1934, 250),
-                new Book("Khalid Hosseini", "The Kite Runner", 433, 11, 0, 2003, 370)
+                new Book("JK Rowling", "Harry Potter and the Philosopher's Stone", 13, 11, 40, 2001, 500,"/images/books/hp.jpg"),
+                new Book("George Orwell", "1984", 43, 11, 30, 1949, 350,"/images/books/hp.jpg"),
+                new Book("J.R.R. Tolkien", "The Lord of the Rings", 12, 11, 20, 1954, 1170,"/images/books/hp.jpg"),
+                new Book("Agatha Christie", "Murder on the Orient Express", 533, 11, 10, 1934, 250,"/images/books/hp.jpg"),
+                new Book("Khalid Hosseini", "The Kite Runner", 433, 11, 0, 2003, 370,"/images/books/hp.jpg")
         ));
 
         List<Movie> movies = new ArrayList<>(Arrays.asList(
-                new Movie("Francis Ford Coppola", "The Godfather", 175, 12, 10, 9.2, 100),
-                new Movie("Quentin Tarantino", "Pulp Fiction", 154, 18, 20, 8.9, 110),
-                new Movie("Christopher Nolan", "Inception", 148, 12, 30, 8.8, 130),
-                new Movie("Steven Spielberg", "Jurassic Park", 127, 7, 0, 8.1, 120)
+                new Movie("Francis Ford Coppola", "The Godfather", 175, 12, 10, 9.2, 100,"/images/books/hp.jpg"),
+                new Movie("Quentin Tarantino", "Pulp Fiction", 154, 18, 20, 8.9, 110,"/images/books/hp.jpg"),
+                new Movie("Christopher Nolan", "Inception", 148, 12, 30, 8.8, 130,"/images/books/hp.jpg"),
+                new Movie("Steven Spielberg", "Jurassic Park", 127, 7, 0, 8.1, 120,"/images/books/hp.jpg")
         ));
 
         List<Music> songs = new ArrayList<>(Arrays.asList(
-                new Music("The Beatles", "Hey Jude", 5, 12, 100, "1970", 200),
-                new Music("Queen", "Bohemian Rhapsody", 6, 12, 50, "1980", 210),
-                new Music("Michael Jackson", "Billie Jean", 4, 12, 25, "2000", 190),
-                new Music("Bob Marley", "No Woman, No Cry", 7, 12, 0, "2010", 180)
+                new Music("The Beatles", "Hey Jude", 5, 12, 100, "1970", 200,"/images/books/hp.jpg"),
+                new Music("Queen", "Bohemian Rhapsody", 6, 12, 50, "1980", 210,"/images/books/hp.jpg"),
+                new Music("Michael Jackson", "Billie Jean", 4, 12, 25, "2000", 190,"/images/books/hp.jpg"),
+                new Music("Bob Marley", "No Woman, No Cry", 7, 12, 0, "2010", 180,"/images/books/hp.jpg")
         ));
 
 
 
         for (Book book : books) {
-            Book bookName = new Book(book.getCreator(),book.getTitle(),book.getPrice(),book.getCost(),book.getAvailableQuantity(),book.getYearOfPublication(),book.getNoOfPages());
+            Book bookName = new Book(book.getCreator(),book.getTitle(),book.getPrice(),book.getCost(),book.getAvailableQuantity(),book.getYearOfPublication(),book.getNoOfPages(),book.getImage());
             productRepository.save(bookName);
         }
 
         for (Movie movie : movies) {
-            Movie movieName = new Movie(movie.getCreator(),movie.getTitle(),movie.getPrice(),movie.getCost(),movie.getAvailableQuantity(),movie.getRating(),movie.getRunTime());
+            Movie movieName = new Movie(movie.getCreator(),movie.getTitle(),movie.getPrice(),movie.getCost(),movie.getAvailableQuantity(),movie.getRating(),movie.getRunTime(), movie.getImage());
             productRepository.save(movieName);
         }
 
         for (Music music : songs) {
-            Music musicName = new Music(music.getCreator(),music.getTitle(),music.getPrice(),music.getCost(),music.getAvailableQuantity(),music.getDecade(),music.getSongLength());
+            Music musicName = new Music(music.getCreator(),music.getTitle(),music.getPrice(),music.getCost(),music.getAvailableQuantity(),music.getDecade(),music.getSongLength(), music.getImage());
             productRepository.save(musicName);
         }
 
