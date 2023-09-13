3f7c339881eaef7e3b958980f81be35cf9f2d2b9
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-13 15:10:27 +0100

    fixed error when clicking filter getbaskettotal would cause program to crash

--- front-end-client/src/components/contexts/ShopContext.js
+++ front-end-client/src/components/contexts/ShopContext.js
@@ -21,6 +21,7 @@ const ShopContextProvider = (props) => {
         12: 0,
         13: 0
     });
+    let basketTotal = 0;
     // adding functionality of adding to basket
     const addToBasket = (itemid) => {
         setBasketItemQuantityList((itemQuantityList) => ({
@@ -40,22 +41,31 @@ const ShopContextProvider = (props) => {
     };
 
     const getBasketTotal = () => {
-        let basketTotal = 0
-        for (
-            const item in basketItemQuantityList
-        ){
-            if (basketItemQuantityList[item] > 0) {
-                 basketTotal += ((basketItemQuantityList[item] * (productItems.find((product) => {
-                    return product.id === Number(item)
-                 })).price) / 100)
+        for (const item in basketItemQuantityList) {
+            if (
+                basketItemQuantityList[item] > 0 &&
+                productItems.find((product) => product.id === Number(item))
+            ) {
+                basketTotal +=
+                    (basketItemQuantityList[item] *
+                        productItems.find((product) => {
+                            return product.id === Number(item);
+                        }).price) /
+                    100;
             }
         }
         return basketTotal.toFixed(2);
-    }
+    };
 
     // create object called context value and add all functions in there
     // makes code more readable
-    const contextValue = { addToBasket, removeFromBasket, productItems, setProductItems, getBasketTotal };
+    const contextValue = {
+        addToBasket,
+        removeFromBasket,
+        productItems,
+        setProductItems,
+        getBasketTotal
+    };
     return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
 };
 
