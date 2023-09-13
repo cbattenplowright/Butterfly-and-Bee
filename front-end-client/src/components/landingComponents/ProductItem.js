c44d76ae8817524ddb0195c481e891d0e703387f
rohaib1 <rohaib192@gmail.com>
2023-09-13 10:29:21 +0100

    Fixed sizing for images

--- front-end-client/src/components/landingComponents/ProductItem.js
+++ front-end-client/src/components/landingComponents/ProductItem.js
@@ -4,7 +4,7 @@ const ProductItem = ({ productItem }) => {
   return (
     <div className="product-item">
       <h4>{productItem.title}</h4>
-      <img src = {productItem.image}/>
+      <img src = {productItem.image} height={"200"}/>
       <p>Price: £{(productItem.price / 100).toFixed(2)}</p>
       <p>Available quantity: {productItem.availableQuantity}</p>
       <button className="add-to-basket">Add to basket</button>
