996e00c746e02d868ca3908b96ae44e1404cea96
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-14 11:31:45 +0100

    created modal component

--- front-end-client/src/components/contexts/ShopContext.js
+++ front-end-client/src/components/contexts/ShopContext.js
@@ -5,139 +5,125 @@ import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-  const [productItems, setProductItems] = useState([]);
-  const [filteredProductItems, setFilteredProductItems] = useState([]);
-  // object that contains state of how many items are in basket per item
-  const [basketItemQuantityList, setBasketItemQuantityList] = useState({
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-  });
-  // adding functionality of adding to basket
-  const addToBasket = (itemid) => {
-    setBasketItemQuantityList((itemQuantityList) => ({
-      // cloning basketListQuantity
-      ...itemQuantityList,
-      [itemid]: itemQuantityList[itemid] + 1,
-    }));
-    console.log(basketItemQuantityList);
-  };
-  const removeFromBasket = (itemid) => {
-    setBasketItemQuantityList((itemQuantityList) => ({
-      // cloning basketListQuantity
-      ...itemQuantityList,
-      [itemid]: itemQuantityList[itemid] - 1,
-    }));
-    console.log(basketItemQuantityList);
-  };
-
-  const getBasketTotal = () => {
-    let basketTotal = 0;
-    for (const item in basketItemQuantityList) {
-      if (
-        basketItemQuantityList[item] > 0 &&
-        productItems.find((product) => product.id === Number(item))
-      ) {
-        basketTotal +=
-          (basketItemQuantityList[item] *
-            productItems.find((product) => {
-              return product.id === Number(item);
-            }).price) /
-          100;
-      }
-    }
-    return basketTotal.toFixed(2);
-  };
-
-  const createOrder = async () => {
-    const orderResponse = await fetch(
-      "http://localhost:8080/customers/addOrder/1",
-      {
-        method: "PUT",
-      }
-    );
-
-    const orderResponseData = await orderResponse.json();
-    const orderId = orderResponseData.id;
-
-    for (let itemid of Object.keys(basketItemQuantityList)) {
-   
-      const quantitySold = basketItemQuantityList[itemid]
-      console.log(itemid);
-      console.log(quantitySold);
-      if (quantitySold > 0){
-        const linkProdOrdersResponse = await fetch(
-            `http://localhost:8080/orders/linkProdOrders/${itemid}/${quantitySold}`,
-            {
-              method: "PUT",
-            }
-          );
-          const linkProdOrdersResponseData = await linkProdOrdersResponse.json();
-          const productOrderId = linkProdOrdersResponseData.id
-
-          const linkOrderProdOrderResponse = await fetch(
-            `http://localhost:8080/orders/linkOrderProdOrder/${orderId}/${productOrderId}`,
-            {
-              method: "PUT",
-            }
-          );
-
-            const linkOrderProdOrderResponseData = await linkOrderProdOrderResponse.json();
-          
-            console.log(linkOrderProdOrderResponseData);
-            console.log(linkProdOrdersResponseData);
-        
-      }
-    
-    }
-
-    setBasketItemQuantityList({
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0,
-      11: 0,
-      12: 0,
-      13: 0,
+    const [productItems, setProductItems] = useState([]);
+    const [filteredProductItems, setFilteredProductItems] = useState([]);
+    // object that contains state of how many items are in basket per item
+    const [basketItemQuantityList, setBasketItemQuantityList] = useState({
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0,
+        11: 0,
+        12: 0,
+        13: 0
     });
+    // adding functionality of adding to basket
+    const addToBasket = (itemid) => {
+        setBasketItemQuantityList((itemQuantityList) => ({
+            // cloning basketListQuantity
+            ...itemQuantityList,
+            [itemid]: itemQuantityList[itemid] + 1
+        }));
+        console.log(basketItemQuantityList);
+    };
+    const removeFromBasket = (itemid) => {
+        setBasketItemQuantityList((itemQuantityList) => ({
+            // cloning basketListQuantity
+            ...itemQuantityList,
+            [itemid]: itemQuantityList[itemid] - 1
+        }));
+        console.log(basketItemQuantityList);
+    };
 
+    const getBasketTotal = () => {
+        let basketTotal = 0;
+        for (const item in basketItemQuantityList) {
+            if (
+                basketItemQuantityList[item] > 0 &&
+                productItems.find((product) => product.id === Number(item))
+            ) {
+                basketTotal +=
+                    (basketItemQuantityList[item] *
+                        productItems.find((product) => {
+                            return product.id === Number(item);
+                        }).price) /
+                    100;
+            }
+        }
+        return basketTotal.toFixed(2);
+    };
 
+    const createOrder = async () => {
+        const orderResponse = await fetch("http://localhost:8080/customers/addOrder/1", {
+            method: "PUT"
+        });
 
-  }
+        const orderResponseData = await orderResponse.json();
+        const orderId = orderResponseData.id;
 
+        for (let itemid of Object.keys(basketItemQuantityList)) {
+            const quantitySold = basketItemQuantityList[itemid];
+            console.log(itemid);
+            console.log(quantitySold);
+            if (quantitySold > 0) {
+                const linkProdOrdersResponse = await fetch(
+                    `http://localhost:8080/orders/linkProdOrders/${itemid}/${quantitySold}`,
+                    {
+                        method: "PUT"
+                    }
+                );
+                const linkProdOrdersResponseData = await linkProdOrdersResponse.json();
+                const productOrderId = linkProdOrdersResponseData.id;
 
-  const contextValue = {
-    addToBasket,
-    removeFromBasket,
-    productItems,
-    setProductItems,
-    getBasketTotal,
-    filteredProductItems,
-    setFilteredProductItems,
-    basketItemQuantityList,
-    createOrder,
-  };
-  return (
-    <ShopContext.Provider value={contextValue}>
-      {props.children}
-    </ShopContext.Provider>
-  );
+                const linkOrderProdOrderResponse = await fetch(
+                    `http://localhost:8080/orders/linkOrderProdOrder/${orderId}/${productOrderId}`,
+                    {
+                        method: "PUT"
+                    }
+                );
+
+                const linkOrderProdOrderResponseData = await linkOrderProdOrderResponse.json();
+
+                console.log(linkOrderProdOrderResponseData);
+                console.log(linkProdOrdersResponseData);
+            }
+        }
+
+        setBasketItemQuantityList({
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0,
+            12: 0,
+            13: 0
+        });
+    };
+
+    const contextValue = {
+        addToBasket,
+        removeFromBasket,
+        productItems,
+        setProductItems,
+        getBasketTotal,
+        filteredProductItems,
+        setFilteredProductItems,
+        basketItemQuantityList,
+        createOrder
+    };
+    return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
 };
 
 export default ShopContextProvider;
--- front-end-client/src/components/modals/OrderConfirmation.js
+++ front-end-client/src/components/modals/OrderConfirmation.js
@@ -1,5 +1,22 @@
 const OrderConfirmation = () => {
-    return <></>;
+    return (
+        <>
+            <section className="modal hidden">
+                <div className="flex">
+                    <button className="btn-close">X</button>
+                </div>
+                <div>
+                    <h3>Order Confirmation:</h3>
+                    {/* Import orderId and customer email from ShopContext */}
+                    <p>Order Id: ${orderId}</p>
+                    <h3>You will receive a order confirmation to: </h3>
+                    <p>{customerEmail}</p>
+                </div>
+            </section>
+            <div className="overlay hidden"></div>
+            <button className="btn btn-open">Open Order Confirmation</button>
+        </>
+    );
 };
 
 export default OrderConfirmation;
