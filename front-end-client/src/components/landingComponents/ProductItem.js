3bec13c1a4c52237c4b0de38d345132aedb59edd
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-14 20:05:21 +0100

    added item quantity display next to item

--- front-end-client/src/components/landingComponents/ProductItem.js
+++ front-end-client/src/components/landingComponents/ProductItem.js
@@ -1,20 +1,30 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../contexts/ShopContext";
-import './ProductList.css'
+import "./ProductList.css";
 
 const ProductItem = ({ productItem }) => {
     // links create context from ShopContext
-    const { addToBasket } = useContext(ShopContext);
+    const { addToBasket, basketItemQuantityList } = useContext(ShopContext);
     return (
         <div className="product-item">
             <h4>{productItem.title}</h4>
             <img src={productItem.image} height={"200"} />
             <p>Price: £{(productItem.price / 100).toFixed(2)}</p>
             <p>Available quantity: {productItem.availableQuantity}</p>
-            <button className="add-to-basket" onClick={() => {
-                addToBasket(productItem.id)}}>
-                Add to basket
-            </button>
+            <div className="basket-button-amount-display">
+                <button
+                    className="add-to-basket"
+                    onClick={() => {
+                        addToBasket(productItem.id);
+                    }}
+                >
+                    Add to basket
+                </button>
+                {/* Renders item quantity amount next to button */}
+                {basketItemQuantityList[productItem.id] > 0 && (
+                    <>{basketItemQuantityList[productItem.id]}</>
+                )}
+            </div>
         </div>
     );
 };
