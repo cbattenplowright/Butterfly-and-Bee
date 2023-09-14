ec20a87419c18e4470c03f3fa9899b87e53fd64e
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-14 22:33:43 +0100

    changed styling of navbar

--- front-end-client/src/components/ShopHeader.js
+++ front-end-client/src/components/ShopHeader.js
@@ -1,37 +1,44 @@
 import { Link } from "react-router-dom";
 import { ShopContext } from "./contexts/ShopContext";
 import { useContext } from "react";
+import "./ShopHeader.css";
 
 const ShopHeader = () => {
     const { getBasketTotal } = useContext(ShopContext);
     return (
         <header>
             {/* <img src="images/icons8-bee-50 (1).png" alt="butterfly and bee logo" height="50" width="50"/> */}
-            <div className ="images">
-            <img src="images/icons8-bee-50 (1).png" alt="butterfly and bee logo" height="50" width="50"/>    
-            <h1>Butterfly & Bee</h1>
-            <img src="images/icons8-butterfly-50 (2).png" alt="butterfly and bee logo" height="50" width="50"/>
+            <div className="images">
+                <img
+                    src="images/icons8-bee-50 (1).png"
+                    alt="butterfly and bee logo"
+                    height="50"
+                    width="50"
+                />
+                <h1>Butterfly & Bee</h1>
+                <img
+                    src="images/icons8-butterfly-50 (2).png"
+                    alt="butterfly and bee logo"
+                    height="50"
+                    width="50"
+                />
             </div>
             <ul id="nav-bar">
-                <li>
+                <li className="menu-icon">
                     <Link to="/">
-                        <i className = "material-icons-home">
-                        <span class="material-symbols-outlined">
-                    home
-                        </span>
+                        <i className="material-icons-home">
+                            <span class="material-symbols-outlined">home</span>
                         </i>
                     </Link>
                 </li>
-                <li>
+                <li className="menu-icon">
                     <Link to="/checkout">
                         <i className="material-icons-basket">
-                        <span class="material-symbols-outlined">
-                                shopping_basket
-                        </span>
-                    </i>
+                            <span class="material-symbols-outlined">shopping_basket</span>
+                        </i>
                     </Link>
                 </li>
-                <li className="basket-total">
+                <li className="basket-total menu-total">
                     <p>£{getBasketTotal()}</p>
                 </li>
             </ul>
