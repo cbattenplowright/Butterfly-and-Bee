5bc226d4aa438279546d904f4c955e7ff258a4d3
RNWalker <rebecca.noelia.walker@gmail.com>
2023-09-14 14:45:25 +0100

    working on CSS in checkout

--- front-end-client/src/components/checkoutComponents/Checkout.js
+++ front-end-client/src/components/checkoutComponents/Checkout.js
@@ -3,18 +3,21 @@ import { ShopContext } from "../contexts/ShopContext";
 import BasketList from "./BasketList";
 import { useNavigate } from "react-router-dom";
 import "./Checkout.css";
-
 const Checkout = () => {
     const navigate = useNavigate();
-    const { productItems, basketItemQuantityList, createOrder, toggleModal } =
-        useContext(ShopContext);
+    const { productItems, basketItemQuantityList, createOrder, toggleModal, getBasketTotal } = useContext(ShopContext);
     return (
-        <div>
+        <div className="checkout-page">
+            <div className = "basket-list">
             <BasketList
                 productItems={productItems}
                 basketItemQuantityList={basketItemQuantityList}
             />
-            <button
+            </div>
+            <div className="order-summary">
+                <h5>Order Summary</h5>
+                <h6>Total: £{getBasketTotal()}</h6>
+                <button className="buy-now-button"
                 onClick={() => {
                     createOrder();
                     navigate("/");
@@ -23,6 +26,7 @@ const Checkout = () => {
             >
                 Buy now!
             </button>
+            </div>
         </div>
     );
 };
