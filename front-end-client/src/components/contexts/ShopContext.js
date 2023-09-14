b1be67450a2c0cdf823343fb24664113a61bd81b
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-14 22:48:31 +0100

    added order id and customer email to order confirmation modal

--- front-end-client/src/components/contexts/ShopContext.js
+++ front-end-client/src/components/contexts/ShopContext.js
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
+    const [orderId, setOrderId] = useState();
+    const [customerEmail, setCustomerEmail] = useState("");
+
     const [modal, setModal] = useState(false);
 
     const toggleModal = () => {
@@ -72,7 +75,8 @@ const ShopContextProvider = (props) => {
         });
 
         const orderResponseData = await orderResponse.json();
-        const orderId = orderResponseData.id;
+        setOrderId(orderResponseData.id);
+        setCustomerEmail(orderResponseData.customer.email);
 
         for (let itemid of Object.keys(basketItemQuantityList)) {
             const quantitySold = basketItemQuantityList[itemid];
@@ -121,9 +125,10 @@ const ShopContextProvider = (props) => {
 
     const updateItemQuantity = (itemQuantity, productId) => {
         setBasketItemQuantityList((itemQuantityList) => ({
-            ...itemQuantityList, [productId]: itemQuantity
-        }))
-    }
+            ...itemQuantityList,
+            [productId]: itemQuantity
+        }));
+    };
 
     const contextValue = {
         addToBasket,
@@ -137,7 +142,9 @@ const ShopContextProvider = (props) => {
         createOrder,
         modal,
         toggleModal,
-        updateItemQuantity
+        updateItemQuantity,
+        orderId,
+        customerEmail
     };
     return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
 };
