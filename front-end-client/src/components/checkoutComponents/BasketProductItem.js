f11ed5e86c9627f89820eab03569e3f3f564b720
RNWalker <rebecca.noelia.walker@gmail.com>
2023-09-14 15:10:53 +0100

    moved product info to the right of the product image in the checkout

--- front-end-client/src/components/checkoutComponents/BasketProductItem.js
+++ front-end-client/src/components/checkoutComponents/BasketProductItem.js
@@ -5,11 +5,13 @@ const BasketProductItem = ({ productItem }) => {
   // links create context from ShopContext
   const { addToBasket } = useContext(ShopContext);
   return (
-    <div className="product-item">
-      <h4>{productItem.title}</h4>
+    <div className="basket-product-item">
       <img src={productItem.image} height={"200"} />
+      <div className ="product-text-info">
+      <h4>{productItem.title}</h4>
       <p>Price: £{(productItem.price / 100).toFixed(2)}</p>
       <p>Available quantity: {productItem.availableQuantity}</p>
+      </div>
       <button
         className="add-to-basket"
         onClick={() => {
